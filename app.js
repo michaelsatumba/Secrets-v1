9a160b9ef29cbd0ea75e240bde6d5c5675365ec5
michaelsatumba <mjsatumba@gmail.com>
2021-09-15 11:53:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -130,6 +130,37 @@ app.get("/secrets", function(req, res) {
   }
 })
 
+<<<<<<< Updated upstream
+=======
+
+app.get("/submit", function(req, res){
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+
+app.post("/submit", function(req, res) {
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function() {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  })
+
+
+
+})
+
+>>>>>>> Stashed changes
 app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
