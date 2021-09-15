44a6771e0d3012332b76507681d7ed7fa8e7adfc
michaelsatumba <mjsatumba@gmail.com>
2021-09-15 13:17:39 -0700

    gitignore level2.5

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -24,8 +24,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "thisisourbigsecret."
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
