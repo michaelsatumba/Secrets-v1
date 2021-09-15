02fcdf5f0284cc319c4c18a113463466279e2402
michaelsatumba <mjsatumba@gmail.com>
2021-09-15 16:30:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -206,8 +206,8 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
-app.listen(3000, function() {
+
+app.listen(port, function() {
   console.log("Server started successfully.");
 });
