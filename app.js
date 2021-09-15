46db5a6c5c985c0fc470f8629f7ffb4a9f511d7f
michaelsatumba <mjsatumba@gmail.com>
2021-09-15 15:37:55 -0700

    Add procfile

--- app.js
+++ app.js
@@ -202,12 +202,12 @@ app.post("/login", function(req, res){
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(port, function() {
+  console.log("Server started successfully.");
 });
